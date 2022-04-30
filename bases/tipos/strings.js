e820037773782033b8d2a001df52649bed13ce42
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-30 15:57:16 -0300

    Test with Strings

---
+++ bases/tipos/strings.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+    var _a;
+    const batman = 'Batman';
+    const linterna = "Linterna";
+    const volcan = `Heroe: Volcan Negro`;
+    console.log(`Soy ${batman}`);
+    console.log(batman.toUpperCase());
+    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No esta pte');
+})();
---
+++ bases/tipos/strings.ts
@@ -0,0 +1,15 @@
+(()=>{
+
+    const batman: string = 'Batman'; 
+    const linterna: string = "Linterna"; 
+    const volcan: string = `Heroe: Volcan Negro`; 
+
+    console.log(`Soy ${batman}`);
+    console.log(batman.toUpperCase());
+    console.log(batman[10]?.toUpperCase() || 'No esta pte');
+
+    
+    
+    
+
+})()
