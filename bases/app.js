ec11417d569f645fa194c16e324ee8fc9885ecec
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-30 17:33:55 -0300

    Exercise

--- bases/app.js
+++ bases/app.js
@@ -1,5 +1,35 @@
 "use strict";
 (() => {
-    const a = 10;
-    console.log(a);
+    //Tipos
+    const batman = 'bruce';
+    const superman = 'clark';
+    const exist = false;
+    //Tuplas
+    const heroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    //Arreglos
+    const aliados = ['WorderWoman', 'Acuaman', 'Sam'];
+    //Enums
+    let strong;
+    (function (strong) {
+        strong[strong["aquaman"] = 0] = "aquaman";
+        strong[strong["batman"] = 1] = "batman";
+        strong[strong["flash"] = 5] = "flash";
+        strong[strong["superman"] = 100] = "superman";
+    })(strong || (strong = {}));
+    const fuerzaAcuaman = strong.aquaman;
+    const fuerzaBatman = strong.batman;
+    const fuerzaFlash = strong.flash;
+    const fuerzaSuperman = strong.superman;
+    //Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio');
+    }
+    //Aserciones de tipo
+    const poder = '100';
+    const largoPoder = poder.length;
+    console.log(largoPoder);
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,8 +1,49 @@
 
 (() =>{
 
-    const a: number = 10; 
-    console.log(a);
+    //Tipos
 
+    const batman: string = 'bruce'; 
+    const superman: string = 'clark'
+
+    const exist: boolean  = false; 
+
+    //Tuplas
+    const heroes: [string, string] = [batman,superman]; 
+    const villano: [string, number, boolean] = ['Lex Lutor', 5, true]; 
+
+    //Arreglos
+    const aliados: (string)[] = ['WorderWoman', 'Acuaman', 'Sam']; 
+
+    //Enums
+    enum strong{
+         aquaman  = 0, 
+         batman = 1, 
+         flash = 5, 
+         superman = 100,
+    }
+    const fuerzaAcuaman = strong.aquaman;  
+    const fuerzaBatman = strong.batman;
+    const fuerzaFlash = strong.flash; 
+    const fuerzaSuperman = strong.superman; 
+
+    
+    //Retorno de funciones
+
+    function activar_batiseñal(): string{
+        return 'activada'; 
+    }
+
+    function pedir_ayuda(): void{
+        console.log('Auxilio');
+    }
+
+    //Aserciones de tipo
+    const poder: any = '100'; 
+    const largoPoder : number = (poder as string).length; 
+    console.log(largoPoder);
+    console.log('Desde aca');
+    
+    
 })()
 
