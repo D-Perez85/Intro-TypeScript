b1d42376a48bb1e86c11b5dcfa92f278fe3d533a
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-30 15:50:11 -0300

    Test with Numbers

---
+++ bases/tipos/numbers.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    let avengers = 10;
+    console.log(avengers);
+    const villians = 20;
+    if (avengers < villians) {
+        console.log('Problemon');
+    }
+    else {
+        console.log('Todo Okardo');
+    }
+    avengers = 123;
+    console.log({ avengers });
+})();
---
+++ bases/tipos/numbers.ts
@@ -0,0 +1,20 @@
+(()=>{
+
+let avengers: number = 10; 
+console.log(avengers);
+
+const villians: number = 20; 
+
+if(avengers < villians){
+    console.log('Problemon');
+    
+}else{
+    console.log('Todo Okardo');
+    
+}
+
+avengers = 123; 
+console.log({avengers});
+
+
+})()
