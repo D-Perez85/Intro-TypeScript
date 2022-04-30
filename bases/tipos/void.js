984a8fe37214150cf221b711610590d8536c1897
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-30 16:48:26 -0300

    Test with Void

---
+++ bases/tipos/void.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    function callHero() {
+        return;
+    }
+    const callHero2 = () => {
+        return;
+    };
+    const a = callHero();
+    console.log(a);
+    const b = callHero2();
+    console.log(b);
+})();
---
+++ bases/tipos/void.ts
@@ -0,0 +1,19 @@
+(()=>{
+
+    function callHero() : void{
+        return; 
+    }
+
+    const callHero2 = () : void =>{
+        return; 
+    }
+
+
+    const a = callHero(); 
+    console.log(a);
+    const b = callHero2(); 
+    console.log(b);
+    
+    
+
+})()
