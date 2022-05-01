9a9f3f7f1f9ce9098c386e7b819bce8cb109c2c1
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-01 15:22:30 -0300

    Let vs Var

---
+++ bases/ES6/let-var.ts
@@ -0,0 +1,12 @@
+(() => {
+
+    //LA IMPORTANCIA DE PONER CONST EN CADA FUNCION
+    const nombre: string = 'Fernando'
+    const getName = (): void => {
+        console.log('viejo getName');
+    }
+
+    // getName = () => { console.log('Nuevo getName') }; //NO SE PUEDE ASIGNAR A GETNAME PORQUE ES UNA CONSTANTE
+    getName()
+
+})()
