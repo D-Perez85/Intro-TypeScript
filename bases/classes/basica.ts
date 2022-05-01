91a06170550aba3742f4ef56b92e0c2ec100b559
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-01 17:00:01 -0300

    Private & Public Methods

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -1,16 +1,23 @@
 (()=>{
 
     class Avenger{
-        //FORMA CORTA DE PASAR PARAMETROS
+        static avgAge: number = 35; 
+        static getAvgAge() {
+            return this.avgAge;
+        }
+         //FORMA CORTA DE PASAR PARAMETROS
         constructor( 
             private name: string, 
             private team: string, 
-            public realName?: string
+            public realName?: string, 
         ){}
+        public bio(){
+            return `${this.name} ${this.team}`
+        }
     }
     const antman : Avenger = new Avenger('Aquaman', 'Azul', 'Jhon Doe'); 
     console.log(antman);
-    
-    
-
+    console.log(antman.bio());
+    console.log( Avenger.getAvgAge() )
 })()
+
--- bases/main.js
+++ bases/main.js
@@ -6,8 +6,17 @@
             this.team = team;
             this.realName = realName;
         }
+        static getAvgAge() {
+            return this.avgAge;
+        }
+        bio() {
+            return `${this.name} ${this.team}`;
+        }
     }
+    Avenger.avgAge = 35;
     const antman = new Avenger('Aquaman', 'Azul', 'Jhon Doe');
     console.log(antman);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
