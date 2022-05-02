c9c63a1e9569b088429acbb085be033aa7c6125c
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-02 18:15:27 -0300

    Generics + Backup

---
+++ ts-node/src/generics/generic.ts
@@ -0,0 +1,15 @@
+export const printObject = (args: any) =>{
+    console.log(args);
+    
+}
+
+export function genericFn <T> (args: T){
+    return args;
+}
+
+export const genericArrowFn = <T> (args: T) =>{
+    return args; 
+    
+}
+
+
